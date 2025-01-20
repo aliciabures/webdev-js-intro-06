270669aea73d98b4028e89afe3d32f75e611d943
Alicia Bures <arosebures@gmail.com>
2025-01-19 21:47:27 -0500

    completed part one and updated readme

--- js/index.js
+++ js/index.js
@@ -1,3 +1,34 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+/*Research the break and the continue keywords. Try to incorporate them in your experimentation.
+Write any questions that may arise in this experiment and we can discuss them in class. */
+
+
+
+
+/*
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+*/
+/*let i = 0;
+while (i < 5) {
+if (i === 3) break;
+    console.log(i);
+    i++;
+}    
+
+  */
+/*
+let text = '';
+
+for (let i = 0; i < 10; i++) {
+  if (i === 3) {
+    continue;
+  }
+  text = text + i;
+  console.log(i)
+}
+  */
